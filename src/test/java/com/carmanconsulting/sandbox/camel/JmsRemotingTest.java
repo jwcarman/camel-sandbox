17c8d346bf34ef756cf01465e613c1683226dba8
James Carman <jcarman@apache.org>
2015-02-03 10:04:17 -0500

    Working with remoting.

--- src/test/java/com/carmanconsulting/sandbox/camel/JmsRemotingTest.java
+++ src/test/java/com/carmanconsulting/sandbox/camel/JmsRemotingTest.java
@@ -4,54 +4,151 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.apache.camel.Endpoint;
 import org.apache.camel.Exchange;
+import org.apache.camel.ExchangeTimedOutException;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.bean.BeanInvocation;
 import org.apache.camel.component.bean.ProxyHelper;
+import org.apache.camel.impl.SimpleRegistry;
 import org.junit.Test;
 
 public class JmsRemotingTest extends JmsTestCase {
-
-    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
-
-    @Test
-    public void testWithCustomSerialization() throws Exception {
-        final Endpoint endpoint = context.getEndpoint("direct:service");
-        final MyService proxy = ProxyHelper.createProxy(endpoint, MyService.class);
-        assertEquals("foo - hello", proxy.foo(new Request("hello")));
-        assertEquals("bar - hello", proxy.bar(new Request("hello")));
-    }
+//----------------------------------------------------------------------------------------------------------------------
+// Other Methods
+//----------------------------------------------------------------------------------------------------------------------
 
     @Override
     protected RouteBuilder createRouteBuilder() throws Exception {
         return new RouteBuilder() {
             @Override
             public void configure() throws Exception {
-                from("direct:service").process(new GsonSerializer()).to("jms:queue:service");
-                from("jms:queue:service").to("log:requests?level=INFO&showAll=true&multiline=true").process(new GsonDeserializer()).bean(EchoService.class);
+                final RuntimeTypeAdapterFactory<Request> requestAdapterFactory = RuntimeTypeAdapterFactory.of(Request.class, "_type");
+                requestAdapterFactory.registerSubtype(FooRequest.class);
+                requestAdapterFactory.registerSubtype(BarRequest.class);
+
+                final RuntimeTypeAdapterFactory<Response> responseAdapterFactory = RuntimeTypeAdapterFactory.of(Response.class, "_type");
+                responseAdapterFactory.registerSubtype(FooResponse.class);
+                responseAdapterFactory.registerSubtype(BarResponse.class);
+
+
+                final Gson gson = new GsonBuilder().registerTypeAdapterFactory(requestAdapterFactory).registerTypeAdapterFactory(responseAdapterFactory).create();
+
+                from("direct:service")
+                        .setBody(simple("${body.args[0]}"))
+                        .process(new GsonSerializer(gson, Request.class))
+                        .to("jms:queue:service?requestTimeout=500")
+                        .process(new GsonDeserializer(gson, Response.class));
+                from("jms:queue:service")
+                        .process(new GsonDeserializer(gson, Request.class))
+                        .bean(new EchoService())
+                        .process(new GsonSerializer(gson, Response.class));
             }
         };
     }
 
-    private static class GsonSerializer implements Processor {
-        @Override
-        public void process(Exchange exchange) throws Exception {
-            final BeanInvocation invocation = exchange.getIn().getBody(BeanInvocation.class);
-            for(int i = invocation.getArgs().length - 1; i >= 0; --i) {
-                invocation.getArgs()[i] = GSON.toJson(invocation.getArgs()[i]);
-            }
+
+    @Override
+    protected void doBindings(SimpleRegistry registry) {
+        registry.put("service", new EchoService());
+    }
+
+    @Test
+    public void testWithCustomSerialization() throws Exception {
+        final Endpoint endpoint = context.getEndpoint("direct:service");
+        final MyService proxy = ProxyHelper.createProxy(endpoint, MyService.class);
+        assertEquals("foo - hello", proxy.foo(new FooRequest("hello")).getValue());
+        assertEquals("bar - hello", proxy.bar(new BarRequest("hello")).getValue());
+    }
+
+    @Test(expected = ExchangeTimedOutException.class)
+    public void testWithException() throws Exception {
+        final Endpoint endpoint = context.getEndpoint("direct:service");
+        final MyService proxy = ProxyHelper.createProxy(endpoint, MyService.class);
+        final FooResponse response = proxy.foo(new FooRequest("bogus"));
+    }
+
+//----------------------------------------------------------------------------------------------------------------------
+// Inner Classes
+//----------------------------------------------------------------------------------------------------------------------
+
+    public static class BarRequest extends Request {
+        public BarRequest(String value) {
+            super(value);
         }
     }
 
-    private static class GsonDeserializer implements Processor {
-        @Override
-        public void process(Exchange exchange) throws Exception {
-            final BeanInvocation invocation = exchange.getIn().getBody(BeanInvocation.class);
-            for(int i = invocation.getArgs().length - 1; i >= 0; --i) {
-                invocation.getArgs()[i] = GSON.fromJson(String.valueOf(invocation.getArgs()[i]), invocation.getMethod().getParameterTypes()[i]);
-            }
+    public static class BarResponse extends Response {
+        public BarResponse(String value) {
+            super(value);
         }
     }
+
+    public class EchoService implements MyService {
+        @Override
+        public FooResponse foo(FooRequest request) {
+            logger.info("Processing foo request...");
+            if("bogus".equals(request.getValue())) {
+                throw new IllegalArgumentException("Not gonna do it!");
+            }
+            return new FooResponse("foo - " + request);
+        }
+
+        @Override
+        public BarResponse bar(BarRequest request) {
+            logger.info("Processing bar request...");
+            return new BarResponse("bar - " + request);
+        }
+    }
+
+    public static class FooRequest extends Request {
+        public FooRequest(String value) {
+            super(value);
+        }
+    }
+
+    public static class FooResponse extends Response {
+        public FooResponse(String value) {
+            super(value);
+        }
+    }
+
+    private class GsonDeserializer implements Processor {
+        private final Gson gson;
+        private final Class<?> type;
+
+        public GsonDeserializer(Gson gson, Class<?> type) {
+            this.gson = gson;
+            this.type = type;
+        }
+
+        @Override
+        public void process(Exchange exchange) throws Exception {
+            logger.info("Deserializing in body to {} object...", type.getCanonicalName());
+            exchange.getIn().setBody(gson.fromJson(exchange.getIn().getBody(String.class), type));
+        }
+    }
+
+    private class GsonSerializer implements Processor {
+        private final Gson gson;
+        private final Class<?> type;
+
+        public GsonSerializer(Gson gson, Class<?> type) {
+            this.gson = gson;
+            this.type = type;
+        }
+
+        @Override
+        public void process(Exchange exchange) throws Exception {
+            logger.info("Serializing in body message...");
+            exchange.getIn().setBody(gson.toJson(exchange.getIn().getBody(type)));
+        }
+    }
+
+    public static interface MyService {
+        public FooResponse foo(FooRequest request);
+
+        public BarResponse bar(BarRequest request);
+    }
+
     public static class Request {
         private final String value;
 
@@ -68,22 +165,25 @@ public class JmsRemotingTest extends JmsTestCase {
         }
     }
 
-    public static class EchoService implements MyService {
-        @Override
-        public String foo(Request request) {
-            return "foo - " + request;
+    public static class Response {
+        private final String value;
+
+        private String errorMessage;
+
+        public Response(String value) {
+            this.value = value;
         }
 
-        @Override
-        public String bar(Request request) {
-            return "bar - " + request;
+        public void setErrorMessage(String errorMessage) {
+            this.errorMessage = errorMessage;
         }
-    }
 
-    public static interface MyService {
+        public String getValue() {
+            return value;
+        }
 
-        public String foo(Request request);
-
-        public String bar(Request request);
+        public String getErrorMessage() {
+            return errorMessage;
+        }
     }
 }
