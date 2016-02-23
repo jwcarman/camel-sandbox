e0cd242de396e771f171feebb412a3a79baadfd2
James Carman <james@carmanconsulting.com>
2016-02-23 10:52:10 -0500

    Creating message groups example.

--- src/test/java/com/carmanconsulting/sandbox/camel/brokerwars/StartBroker2.java
+++
@@ -1,7 +0,0 @@
-package com.carmanconsulting.sandbox.camel.brokerwars;
-
-public class StartBroker2 {
-    public static void main(String[] args) throws Exception {
-        StartBroker.startBroker(12346, 12345);
-    }
-}
